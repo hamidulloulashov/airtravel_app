961962d4f013afef4018593b43413b8ff1f0cbba
Hamidullo Ulashov <ulashovhamidullo.com>
2025-10-28 06:28:24 -0700

    Hamidullo

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -9,11 +9,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.containerBlue,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text("Meni yoz!!!", style: TextStyle(color: Colors.white, fontSize: 40),))
-        ],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.canPop(context);
+          },
+        ),
+        backgroundColor: AppColors.containerBlue, 
+        elevation: 0,
+      ),
+      body: Center(
+        child: Text(
+          "Meni yoz!!!",
+          style: TextStyle(color: Colors.white, fontSize: 40),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBarApp(),
     );
