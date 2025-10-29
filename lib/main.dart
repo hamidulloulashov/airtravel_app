724240f4f5806e5145e60b3d4f11ce3bed2cf1b7
Hamidullo Ulashov <ulashovhamidullo.com>
2025-10-29 10:19:55 -0700

    Hamidullo

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:airtravel_app/core/router/router.dart' as AppRouter;
 import 'package:airtravel_app/core/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 void main(List<String> args) {
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
